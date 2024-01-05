3af3c78d48bec1d466729e18a47c760c70097b48
claranceJ <77950647+claranceJ@users.noreply.github.com>
2024-01-05 18:39:28 +0000

    updated the compound.py

--- compound.py
+++ compound.py
@@ -1,20 +1,38 @@
+#Compound Datastructure
 elements = {
-  "helium": {
-    "weight": 4.0026,
-    "group": "noble gases",
-    "symbol": "He"
-  },
-  "Hydrogen":{
-    "weight": 1.0078,
-    "group": "alkali metals",
-    "symbol": "H"
-  },
-  "Lithium":{
-    "weight": 6.94,
-    "group": "alkali metals",
-    "symbol": "Li"
-  }
+    "helium": {
+        "weight": [4.0026, 32],
+        "group": "noble gases",
+        "symbol": "He"
+    },
+    "Hydrogen": {
+        "weight": 1.0078,
+        "group": "alkali metals",
+        "symbol": "H"
+    },
+    "Lithium": {
+        "weight": 6.94,
+        "group": "alkali metals",
+        "symbol": "Li"
+    }
 }
 
+# printing the elements value and checking if it exists
 print(elements["helium"]["weight"])
 print(elements.get("Oxygen", "There\'s no Oxygen in the dictionary"))
+
+#Adding Elements in a dictionary
+Oxygen = {"weight": 15.999, "group": "halogens", "symbol": "O"}
+elements["Oxygen"] = Oxygen
+
+print("elements: ", elements)
+
+#OR
+
+elements["Sodium"] = {"weight": 23.0, "group": "alkali metals", "symbol": "Na"}
+
+print("updated list: ", elements)
+
+#Appending Elements in a dictionary
+elements["helium"]["weight"].append(21)
+print(elements["helium"]["weight"])
